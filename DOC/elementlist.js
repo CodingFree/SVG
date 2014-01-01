d055faf11bbdc016c517b1678764f9c1f5d473af
CodingFree <madrid.crespo@gmail.com>
2014-01-01 17:28:09 +0100

    Documentation

---
+++ DOC/elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","svgCanvas"],["c","svgCircle"],["c","svgEllipse"],["c","svgObject"],["c","svgRectangle"],["c","svgSquare"],["c","svgText"],["c","svgTextBox"]];
